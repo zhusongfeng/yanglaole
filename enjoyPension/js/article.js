60304a333fb4f8c7af2f1caccd858d0fbe1be439
zhusongfeng <1114853779@qq.com>
2018-12-04 13:37:23 +0800

    优化17

--- enjoyPension/js/article.js
+++ enjoyPension/js/article.js
@@ -1,56 +1,3 @@
-/*
-//展示阅读全文
-$('.mp-article-content-more-wrap').click(function () {
-    $('.mp-article-images').show();
-    $('.mp-article-texts').show();
-    $(this).hide();
-});
-//关注
-$('.mp-author-link-hand').click(function () {
-    alert('功能尚未开通，后期完善。敬请你的期待！')
-});
-//解决音乐自动播放
-/!*$(document).ready(function () {
-    $('html').one('touchstart',function(){
-        audio.play();
-        $('.mp-article-music-static-hand i').addClass('mp-article-music-static-pause').removeClass('mp-article-music-static-play')
-    });
-});*!/
-
-//控制音乐按钮的显示或隐藏
-$(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 500) {
-            $('.mp-article-music-migu-fixed').show();
-        } else {
-            $('.mp-article-music-migu-fixed').hide();
-        }
-    });
-});
-
-//控制音乐播放或暂停
- $('.mp-article-music-migu-fixed').click(function () {
-     if(audio.paused) {
-         $(this).addClass('animation');
-         $('.mp-article-music-static-hand i').addClass('mp-article-music-static-pause').removeClass('mp-article-music-static-play');
-         audio.play();
-     } else {
-         $(this).removeClass('animation');
-         $('.mp-article-music-static-hand i').addClass('mp-article-music-static-play').removeClass('mp-article-music-static-pause');
-         audio.pause();
-     }
- });
-//输入框
-$('.comment-input').focus(function () {
-    $('.mp-article-foots').show();
-    $('.mp-article-foot').hide();
-});
-//点击发送
-$('.input-send').click(function () {
-    $('.mp-article-foot').show();
-    $('.mp-article-foots').hide();
-});
-*/
 
  let  comment = new Vue({
     el: '#app',
@@ -100,6 +47,7 @@ $('.input-send').click(function () {
                 token: self.articleToken,
                 article_id: self.articleID ,
             }, function (res) {
+                alert(111,res)
                 if(res.code == 200){
                     if(res.data.type == 1){
                         self.dataType = res.data.type;
